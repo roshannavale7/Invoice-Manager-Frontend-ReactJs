36b20ca46b9a83461fe3a20402ef46782a83c866
Swapnil Patil <81896679+sapatil0987@users.noreply.github.com>
2021-11-29 19:25:25 +0530

    Revert "adding homepage and dashboard code"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import "./app.css"
-import Dashboard from "./components/dashboard/Dashboard";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div><Topbar/>
-  <div className="container">
-    <Sidebar/>
-    <Dashboard/>
-  
-  </div>
-
-  </div>;
-  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Js
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
